d0ff929d7f2004a19de8e78d2016cd2319072163
luigirsan1 <luigirsan1@alum.us.es>
2024-02-19 17:10:18 +0100

    [refactor]: Cambios de la entidad ProgressLog,#12

---
+++ src/main/java/acme/entities/progressLog/ProgressLog.java
@@ -0,0 +1,63 @@
+
+package acme.entities.progressLog;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.data.AbstractEntity;
+import acme.entities.contract.Contract;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(indexes = {
+	@Index(columnList = "record_id"), //
+})
+public class ProgressLog extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "^PG-[A-Z]{1,2}-[0-9]{4}$", message = "{validation.ProgressLog.recordId}")
+	private String				recordId;
+
+	@Positive
+	private Integer				completenessPercentage;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				progressComment;
+
+	@NotNull
+	private Date				registrationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				registrationResponsible;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@ManyToOne(optional = false)
+	private Contract			contract;
+}
