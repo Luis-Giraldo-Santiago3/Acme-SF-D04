9bc940becccf839762203e026f74256f6888c848
DP2 user <DP2 user@10.100.168.92>
2024-02-21 11:42:21 +0100

    [WIP]:Entity code audit

---
+++ src/main/java/acme/entities/codeAudits/codeAudit.java
@@ -0,0 +1,21 @@
+
+package acme.entities.codeAudits;
+
+import javax.persistence.Entity;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class codeAudit extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long serialVersionUID = 1L;
+
+	//Atributes --------------------------------------------------------------
+
+}
