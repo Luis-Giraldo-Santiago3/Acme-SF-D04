432881c1c9016a857e742823db881277d0249262
DP2 user <DP2 user@LAPTOP-RAFITOL-ELTROL>
2024-02-22 11:47:40 +0100

    [WIP]: CodeAudit class

---
+++ src/main/java/acme/entities/codeAudits/CodeAudit.java
@@ -0,0 +1,50 @@
+
+package acme.entities.codeAudits;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class CodeAudit extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes --------------------------------------------------------------
+
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	@NotBlank
+	@Column(unique = true)
+	private String				code;
+
+	@Column(nullable = false)
+	@Past
+	private Date				executionDate;
+
+	@Column(nullable = false)
+	private Type				type;
+
+	@NotBlank
+	@Size(max = 100)
+
+	private List<String>		correctiveActions;
+
+	@Column(nullable = false)
+	private Mark				mark;
+
+	private String				link;
+}
