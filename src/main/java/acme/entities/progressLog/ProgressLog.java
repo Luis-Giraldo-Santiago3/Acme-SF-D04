3d4081ed2fd2d653f8e9158ac5d70d8a2c102934
luigirsan1 <luigirsan1@alum.us.es>
2024-03-06 16:18:18 +0100

    Fix: Cambio en los atributos
    Se cambia las restricciones de los atributos
    
    Refs #12

--- src/main/java/acme/entities/progressLog/ProgressLog.java
+++ src/main/java/acme/entities/progressLog/ProgressLog.java
@@ -5,10 +5,13 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -25,10 +28,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(indexes = {
-	@Index(columnList = "draftMode"), //
-	@Index(columnList = "recordId")
-})
+@Table
 public class ProgressLog extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
@@ -42,21 +42,24 @@ public class ProgressLog extends AbstractEntity {
 	@Pattern(regexp = "^PG-[A-Z]{1,2}-[0-9]{4}$", message = "{validation.ProgressLog.recordId}")
 	private String				recordId;
 
+	@NotNull
 	@Positive
+	@Min(0)
+	@Max(1)
 	private double				completeness;
 
 	@NotBlank
 	@Length(max = 100)
 	private String				comment;
 
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
 	@Past
 	private Date				registrationMoment;
 
 	@NotBlank
 	@Length(max = 75)
 	private String				responsiblePerson;
-
-	private boolean				draftMode;
 
 	// Derived attributes -----------------------------------------------------
 
