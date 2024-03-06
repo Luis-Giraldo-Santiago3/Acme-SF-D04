8955fcec9873953820fbbd34432547c47480b635
luigirsan1 <luigirsan1@alum.us.es>
2024-03-06 16:18:10 +0100

    Fix: Cambio en los atributos
    Se cambia las restricciones de los atributos
    
    Refs #11

--- src/main/java/acme/entities/contract/Contract.java
+++ src/main/java/acme/entities/contract/Contract.java
@@ -5,9 +5,11 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -19,17 +21,14 @@ import org.hibernate.validator.constraints.Length;
 import acme.client.data.AbstractEntity;
 import acme.client.data.datatypes.Money;
 import acme.entities.project.Project;
+import acme.roles.Client;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@Table(indexes = {
-	@Index(columnList = "draftMode"), //
-	@Index(columnList = "code")
-
-})
+@Table
 public class Contract extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
@@ -43,6 +42,8 @@ public class Contract extends AbstractEntity {
 	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$", message = "{validation.Contract.code}")
 	private String				code;
 
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
 	@Past
 	private Date				instantiationMoment;
 
@@ -58,10 +59,9 @@ public class Contract extends AbstractEntity {
 	@Length(max = 100)
 	private String				goals;
 
+	@NotNull
 	@Valid
 	private Money				budget;
-
-	private boolean				draftMode;
 
 	// Derived attributes -----------------------------------------------------
 
@@ -71,4 +71,9 @@ public class Contract extends AbstractEntity {
 	@Valid
 	@ManyToOne(optional = false)
 	private Project				project;
+
+	@NotNull
+	@Valid
+	@OneToOne(optional = false)
+	private Client				client;
 }
