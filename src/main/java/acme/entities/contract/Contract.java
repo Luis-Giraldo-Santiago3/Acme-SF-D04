4158c292667857d3b0009520c047728ee49c2165
luigirsan1 <luigirsan1@alum.us.es>
2024-03-06 16:34:07 +0100

    Fix: Cambia restricciones
    Se cambioa alas restricciones de los atributos
    
    Refs #11

--- src/main/java/acme/entities/contract/Contract.java
+++ src/main/java/acme/entities/contract/Contract.java
@@ -11,12 +11,12 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
-
-import org.hibernate.validator.constraints.Length;
 
 import acme.client.data.AbstractEntity;
 import acme.client.data.datatypes.Money;
@@ -44,19 +44,22 @@ public class Contract extends AbstractEntity {
 
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
-	@Past
+	@PastOrPresent
 	private Date				instantiationMoment;
 
 	@NotBlank
-	@Length(max = 75)
+	@Min(0)
+	@Max(75)
 	private String				providerName;
 
 	@NotBlank
-	@Length(max = 75)
+	@Min(0)
+	@Max(75)
 	private String				customerName;
 
 	@NotBlank
-	@Length(max = 100)
+	@Min(0)
+	@Max(100)
 	private String				goals;
 
 	@NotNull
