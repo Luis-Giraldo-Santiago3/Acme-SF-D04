68aa1ffdf96fdd314ee4e5ac38445848f9aba8e2
DP2 user <DP2 user@LAPTOP-RAFITOL-ELTROL>
2024-03-07 19:07:00 +0100

    [FIX]: Fixed some annotations

--- src/main/java/acme/entities/auditRecords/AuditRecord.java
+++ src/main/java/acme/entities/auditRecords/AuditRecord.java
@@ -8,10 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
@@ -34,24 +37,27 @@ public class AuditRecord extends AbstractEntity {
 	@Column(unique = true)
 	private String				code;
 
-	@Column(nullable = false)
+	@NotNull
 	@Past
-	@Temporal(TemporalType.TIME)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date				auditPeriodStart;
 
-	@Column(nullable = false)
+	@NotNull
 	@Past
-	@Temporal(TemporalType.TIME)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date				auditPeriodEnd;
 
-	@Column(nullable = false)
+	@NotNull
 	private Mark				mark;
 
 	@URL
+	@Length(max = 255)
 	private String				link;
 
 	// Relationships ----------------------------------------------------------
 
+	@NotNull
+	@Valid
 	@ManyToOne(optional = false)
 	private CodeAudit			codeAudit;
 
