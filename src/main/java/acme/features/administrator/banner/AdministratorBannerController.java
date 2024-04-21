adfbd19c9cc67e8b2a7c1034eae813e2e7711392
davvarmun <davidcolegalaroza@gmail.com>
2024-04-21 23:56:16 +0200

    fix: Lista banners mejora

---
+++ src/main/java/acme/features/administrator/banner/AdministratorBannerController.java
@@ -0,0 +1,31 @@
+
+package acme.features.administrator.banner;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.client.controllers.AbstractController;
+import acme.client.data.accounts.Administrator;
+import acme.entities.group.Banner;
+
+@Controller
+public class AdministratorBannerController extends AbstractController<Administrator, Banner> {
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorBannerShowService	showService;
+
+	@Autowired
+	private AdministratorBannerListService	listService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		//super.addBasicCommand("show", this.showService);
+		super.addCustomCommand("list-service", "list", this.listService);
+	}
+}
