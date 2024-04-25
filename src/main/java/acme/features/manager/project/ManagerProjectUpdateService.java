cd1d38d478cc7062e03ebcabbd99eb4f802e52ad
DiViToXiC <davguifer@alum.us.es>
2024-04-25 13:23:10 +0200

    add become manager

--- src/main/java/acme/features/manager/project/ManagerProjectUpdateService.java
+++ src/main/java/acme/features/manager/project/ManagerProjectUpdateService.java
@@ -50,7 +50,7 @@ public class ManagerProjectUpdateService extends AbstractService<Manager, Projec
 	public void bind(final Project object) {
 		assert object != null;
 
-		super.bind(object, "code", "title", "projectAbstract", "fatalErrors", "cost", "link", "published");
+		super.bind(object, "code", "title", "projectAbstract", "fatalErrors", "cost", "link");
 
 	}
 
