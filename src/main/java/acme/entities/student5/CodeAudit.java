4e94ef01cb731a7cc03072e74e33d41d70a1e929
DP2 user <DP2 user@LAPTOP-RAFITOL-ELTROL>
2024-05-19 18:40:34 +0200

    [Fix]: Become an Auditor now available

--- src/main/java/acme/entities/student5/CodeAudit.java
+++ src/main/java/acme/entities/student5/CodeAudit.java
@@ -37,7 +37,7 @@ public class CodeAudit extends AbstractEntity {
 
 	// Attributes --------------------------------------------------------------
 
-	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}", message = "{validation.codeAudit.code}")
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}", message = "validation.codeAudit.code")
 	@NotBlank
 	@NotNull
 	@Column(unique = true)
